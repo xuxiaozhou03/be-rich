ff7a3a71e68f17869e64609a74954f2621f1bde8
allen <xuxiaozhou@oschina.cn>
2025-06-03 21:47:28 +0800

    v1.1

--- app/content.tsx
+++ app/content.tsx
@@ -43,14 +43,42 @@ const orginGroup = (list as unknown as Etf[]).reduce((acc, etf) => {
 const groupList = Object.values(orginGroup).map((group) => {
   group.list.sort((a, b) => b.scale - a.scale);
   return group;
-});
+}).filter((group) => group.list[0].scale > 2);
 const Content: React.FC = () => {
   return (
-    <div className="">
-      {groupList.map((group) => (
+    <table className="border border-slate-200 w-full">
+      {/* {groupList.map((group) => (
         <Group key={group.trackingTarget} group={group} />
-      ))}
-    </div>
+      ))} */}
+      <thead>
+        <tr className="border-b border-slate-200">
+          <th className="border-r border-slate-200">跟踪指数</th>
+          <th className="border-r border-slate-200">基准</th>
+          <th className="border-r border-slate-200">ETF名称</th>
+          <th className="border-r border-slate-200">代码</th>
+          <th className="border-r border-slate-200">规模</th>
+        </tr>
+      </thead>
+      <tbody>
+        {groupList.map((group) => (
+          <React.Fragment key={group.trackingTarget}>
+            <tr className="border-b border-slate-200">
+              <td className="border-r border-slate-200">{group.trackingTarget}</td>
+              <td className="border-r border-slate-200">{group.list[0].benchmark}</td>
+              <td className="border-r border-slate-200">{group.list[0].name}</td>
+              <td className="border-r border-slate-200">{group.list[0].symbol}</td>
+              <td>{group.list[0].scale}</td>
+            </tr>
+            {/* {group.list.slice(1).map((etf) => (
+              <tr key={etf.symbol} className="border-b border-slate-200">
+                <td className="border-r border-slate-200">{etf.name}</td>
+                <td className="border-r border-slate-200">{etf.scale}</td>
+              </tr>
+            ))} */}
+          </React.Fragment>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
